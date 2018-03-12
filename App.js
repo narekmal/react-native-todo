ec88628cb35cfd079899a6fde685be45f6bcd00e
Narek Malkhasyan <narek.mal@gmail.com>
2018-03-12 21:08:54 +0400

    first commit - create-react-native-app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the menu.</Text>
+        <TouchableHighlight><Text>in touchablenew</Text></TouchableHighlight>
+        <TextInput>something</TextInput>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
